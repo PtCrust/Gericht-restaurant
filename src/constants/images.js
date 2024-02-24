2e1ad67efe2f1757ad9a62980439373a7bfae6ba
Tony <61390876+PtCrust@users.noreply.github.com>
2024-02-24 19:20:07 +0200

    Add files via upload

--- src/constants/images.js
+++ src/constants/images.js
@@ -21,7 +21,8 @@ import sign from '../assets/sign.png';
 import quote from '../assets/quote.png';
 import gericht from '../assets/gericht.png';
 
-export default {
+
+const images = {
   bg,
   chef,
   G,
@@ -43,5 +44,6 @@ export default {
   award05,
   sign,
   quote,
-  gericht,
-};
+  gericht
+}
+export default images;
